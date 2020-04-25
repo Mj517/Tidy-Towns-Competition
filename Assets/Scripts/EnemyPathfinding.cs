54885c758508d8876f9394855bc4774a213b73fb
Mj517 <eoin@philgra.org>
2020-04-25 15:17:54 +0100

    25/04/20

---
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    public Transform Player;
+    public float speed = 2f;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndofPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+    public float nextWaypointDistance = 3f;
+    public float distanceFromPlayer;
+    public float enemyH;
+    public GameObject enemy;
+    
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+
+
+
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        seeker.StartPath(rb.position, Player.position, OnPathComplete);
+    }
+       
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+        
+            return;
+        
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndofPath = true;
+            return;
+        }else
+        {
+            reachedEndofPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        if (distanceFromPlayer <= 10)
+        {
+            rb.AddForce(force);
+        }
+
+
+
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+    
+    }
+
+    private void Update()
+    {
+        
+        distanceFromPlayer = Vector2.Distance(rb.position, Player.position);
+        enemyH = (enemy.GetComponent<EnemyDeath>().enemyHealth);
+
+        if (enemyH == 0)
+        {
+            Destroy(gameObject);
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
+        
+    
+        
+    
+
---
+++ Assets/Scripts/EnemyRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotation : MonoBehaviour
+{
+    public Transform Player;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 lookDirection = Player.position - transform.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        transform.eulerAngles = new Vector3(0, 0, angle -180);
+
+    }
+
+    
+}
